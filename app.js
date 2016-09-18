19e448ac100c9c749cac182c2d1ccdf413f610e1
sameeram <mgjaye@gmail.com>
2016-09-18 21:36:49 +0530

    Cross origin headers

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var stories = utils.loadStories();
 
 cache.put('stories',stories);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/v1/stories',function(req,res) {
     var msg = {};
     msg.status = 200;
